55b3f5c5e7ceba5facfbdb2a372c5b727153e0cd
leonardoalvarez20 <leonardo.alvarez.dev.20@gmail.com>
2024-05-04 23:57:14 -0700

    ci: run test workflow

--- src/ecommerce-web-app/src/App.js
+++ src/ecommerce-web-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React122
+          Learn React1222
         </a>
       </header>
     </div>

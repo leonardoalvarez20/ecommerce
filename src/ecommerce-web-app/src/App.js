48c3f07d14be067f26984ace1f49c4c60d6b96d9
leonardoalvarez20 <117400391+leonardoalvarez20@users.noreply.github.com>
2024-05-05 00:16:53 -0700

    ci: run test gh (#43)
    * chore: run workflow
    
    -Fixes #38
    
    * chore: update lock file
    
    * ci: run test gh

--- src/ecommerce-web-app/src/App.js
+++ src/ecommerce-web-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React1222
+          Learn React12222
         </a>
       </header>
     </div>
--- src/ecommerce-web-app/src/App.test.js
+++ src/ecommerce-web-app/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react1222/i);
+  const linkElement = screen.getByText(/learn react12222/i);
   expect(linkElement).toBeInTheDocument();
 });
